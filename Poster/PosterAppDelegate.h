db0c6e41e2a8f586f947962c5ce49cb8c52ff852
David Joerg <dsjoerg@gmail.com>
2014-02-11 10:12:38 -0500

    Initial Commit

---
+++ Poster/PosterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PosterAppDelegate.h
+//  Poster
+//
+//  Created by David Joerg on 2/11/14.
+//  Copyright (c) 2014 David Joerg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PosterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Poster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Poster
+//
+//  Created by David Joerg on 2/11/14.
+//  Copyright (c) 2014 David Joerg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PosterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PosterAppDelegate class]));
+    }
+}
